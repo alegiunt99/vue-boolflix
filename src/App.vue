eee2d7713913315b348e2c15b779c337bf2f4bbb
Alessandro Giunta <alegiunt99@gmail.com>
2022-04-20 12:21:01 +0200

    Ho creato i file per header e main

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- Header con logo e searchbar -->
+    <HeaderComp />
+
+    <!-- contenitore delle card di film e serie ricercati -->
+    <MainComp/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import HeaderComp from '@/components/HeaderCom.vue';
+
+import MainComp from '@/components/MainComp.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
+    HeaderComp,
+
+    MainComp
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/HeaderCom.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'HeaderComp',
+}
+</script>
+
+<style lang='scss' scoped>
+
+</style>
