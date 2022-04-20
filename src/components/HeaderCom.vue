e218ed425138b5829d948b0d973b1e2e8e41b56b
Alessandro Giunta <alegiunt99@gmail.com>
2022-04-20 12:32:18 +0200

    Ho inserito il logo

--- src/components/HeaderCom.vue
+++ src/components/HeaderCom.vue
@@ -1,6 +1,13 @@
 <template>
-  <div>
-  </div>
+  <header>
+      <div id="logo-box">
+          <img src="@/assets/netflix_logo.png" alt="logo">
+      </div>
+
+      <div id="search-box">
+          
+      </div>
+  </header>
 </template>
 
 <script>
@@ -10,5 +17,15 @@ export default {
 </script>
 
 <style lang='scss' scoped>
+
+    header{
+        display: flex;
+        #logo-box{
+            width: 150px;
+            img{
+                width: 100%;
+            }
+        }
+    }
 
 </style>
